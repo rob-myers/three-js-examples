dd92feb18662748fd56e1907b5d191ebdeca8614
Rob Myers <me.robmyers@gmail.com>
2024-04-30 08:42:43 +0100

    feat: basic react-three-fiber example

--- vite-react/src/App.tsx
+++ vite-react/src/App.tsx
@@ -1,33 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
+import { Canvas } from '@react-three/fiber';
+import { PerspectiveCamera, OrbitControls } from '@react-three/drei';
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        Hello, World!
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
+      <div style={{ width: 600, height: 400, border: '1px solid #888' }}>
+        <Canvas>
+          <PerspectiveCamera/>
+          <OrbitControls/>
+          <mesh>
+            <boxGeometry/>
+            <meshBasicMaterial color="red" />
+          </mesh>
+        </Canvas>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
