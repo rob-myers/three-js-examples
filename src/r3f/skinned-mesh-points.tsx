9920def6b13abc237f114a3d57e5126bbe5b673b
robmyers <me.robmyers@gmail.com>
2024-05-02 12:44:38 +0100

    feat: used `npx gltfjsx base-mesh-246-tri.glb  --transform`

--- src/r3f/skinned-mesh-points.tsx
+++ src/r3f/skinned-mesh-points.tsx
@@ -1,12 +1,12 @@
-import { OrbitControls, useGLTF } from '@react-three/drei';
+import { OrbitControls } from '@react-three/drei';
+import { BaseMesh246TriModel } from './Base-mesh-246-tri';
 // import * as THREE from 'three';
 
 export default function SkinnedMeshPoints() {
 
   // const {scene: model } = useGLTF('/assets/FinalBaseMesh.glb');
-  const {scene: model } = useGLTF('/assets/base-mesh-246-tri.glb');
-
-  console.log(model)
+  // const {scene: model } = useGLTF('/assets/base-mesh-246-tri.glb');
+  // console.log(model)
 
   return <>
     <OrbitControls/>
@@ -15,8 +15,9 @@ export default function SkinnedMeshPoints() {
       <boxGeometry />
       <meshBasicMaterial color="gray" />
     </mesh>
-    <group>
+    <BaseMesh246TriModel/>
+    {/* <group>
       <primitive object={model} />
-    </group>
+    </group> */}
   </>
 }
